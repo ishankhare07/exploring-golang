598a994c995691109f80802d47d415bc74bbae6b
Ishan Khare <ishankhare07@gmail.com>
2015-08-19 14:44:24 +0530

    added type conversion code to the average program

--- chapter6/average.go
+++ chapter6/average.go
@@ -6,10 +6,10 @@ func main() {
     var arr [5]float64
     var total float64 = 0
 
-    for i := 0; i < 5; i++ {
+    for i := 0; i < len(arr); i++ {
         fmt.Scanf("%f", &arr[i])
         total += arr[i]
     }
 
-    fmt.Println("Average =", total/5)
+    fmt.Println("Average =", total/float64(len(arr)))
 }
